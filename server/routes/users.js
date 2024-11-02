4234b650c16f2ab28aad86a7fb41b2b330ae8243
Shawn Gestupa <thereal.nwahs@gmail.com>
2024-11-02 10:50:14 +0800

    refactor user registration to include auto-creation of chatbot room for a user

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,21 +1,36 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require('mongoose');
+
 const User = require("../models/user"); // Ensure the User model is correctly set up
+const Chatroom = require("../models/chatroom");
+const ChatroomMembers = require("../models/chatroomMembers");
 
 // POST route to handle user registration
 router.post("/", async (req, res) => {
   try {
     // Check if user already exists
     const { email } = req.body;
-    let user = await User.findOne({ email });
-    if (user) return res.status(400).send({ message: "User already exists" });
+    const userExists = await User.findOne({ email });
+    
+    if (userExists) 
+      return res.status(400).send({ message: "User already exists" });
 
     // Save new user with role
-    user = new User(req.body);
-    await user.save();
-    res.status(201).send({ message: "User registered successfully" });
+    const user = await new User(req.body).save();
+
+    if (user) {
+      const chatroom = await new Chatroom().save();
+
+      new ChatroomMembers({ chatroomId: chatroom["_id"], userId: user["_id"]}).save();
+      new ChatroomMembers({ chatroomId: chatroom["_id"], userId: new mongoose.Types.ObjectId("67249b84b01d71f586fdca22") }).save();
+
+      return res.status(201).send({ message: "User registered successfully" });
+    }
+
+    return res.status(500).send({ message: "Something went wrong! Please try again." });
   } catch (error) {
-    res.status(500).send({ message: "Internal Server Error" });
+    return res.status(500).send(error);
   }
 });
 
