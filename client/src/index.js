046fb04da8dbd3cf723b5654b5b288c6c922cbca
Shawn Gestupa <thereal.nwahs@gmail.com>
2024-11-03 14:20:39 +0800

    prevent react from re-rendering components twice

--- client/src/index.js
+++ client/src/index.js
@@ -8,11 +8,9 @@ const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <App />
-      </UserProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <UserProvider>
+      <App />
+    </UserProvider>
+  </BrowserRouter>
 );
